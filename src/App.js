8a782f2334c87bfa318a8b444136ae3944c1f442
Jid Kimachia <kimachiajidraf@gmail.com>
2025-10-20 08:08:53 +0300

    fix: route /phones now correctly renders Phones component without showing on /

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Phones from "./pages/Phones";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Phones />} />
+        {/* Only render Phones when URL is /phones */}
+        <Route path="/phones" element={<Phones />} />
+        <Route path="/" element={<h1 className="text-center mt-10">Welcome Home</h1>} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
